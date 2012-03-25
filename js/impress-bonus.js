e1d0a3fddc35f43a96608a62d5a7ea5bd8ed92d2
zeke <zeke@sikelianos.com>
2012-03-24 22:17:23 -0700

    Remove presentational aspect from Step class

--- js/impress-bonus.js
+++ js/impress-bonus.js
@@ -2,16 +2,10 @@
 
   window.Step = (function() {
 
-    function Step(content, properties) {
-      this.content = content;
+    function Step(id, properties) {
+      var key, value, _ref;
+      this.id = id;
       this.properties = properties != null ? properties : {};
-      window.steps || (window.steps = []);
-      steps.push(this);
-    }
-
-    Step.prototype.dataProps = function() {
-      var key, morsels, value, _ref;
-      morsels = [];
       _ref = this.properties;
       for (key in _ref) {
         value = _ref[key];
@@ -19,15 +13,9 @@
         if (key === 'rotateY') key = 'rotate-y';
         if (key === 'rotateZ') key = 'rotate-z';
         if (key === 'transitionDuration') key = 'transition-duration';
-        if (key !== 'class') morsels.push("data-" + key + "='" + value + "'");
+        $("#" + this.id).data(key, value);
       }
-      console.log(morsels.join(" "));
-      return morsels.join(" ");
-    };
-
-    Step.prototype.to_html = function() {
-      return "<div class='step " + this.properties["class"] + "' " + (this.dataProps()) + ">" + this.content + "</div>";
-    };
+    }
 
     return Step;
 
@@ -38,12 +26,6 @@
     function Impress() {}
 
     Impress.run = function() {
-      var step, _i, _len;
-      $('body').add('div').attr('id', 'impress');
-      for (_i = 0, _len = steps.length; _i < _len; _i++) {
-        step = steps[_i];
-        $("#impress").append(step.to_html());
-      }
       return loadImpress(document, window);
     };
 
